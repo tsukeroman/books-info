cebbdbd40f5174b18d3c7e0d3232e3d774f3dc8c
tsukeroman <tsukeroman@gmail.com>
2018-10-14 19:25:52 +0300

    app is connected and successfuly retrieves data from goodreads API

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const debug = require('debug')('app');
 const path = require('path');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+const axios = require('axios');
+const xml2js = require('xml2js');
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -12,6 +14,7 @@ const connection = mysql.createConnection({
   password: 'root',
   database: 'books-info'
 });
+const parser = xml2js.Parser({ explicitArray: false });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -34,6 +37,34 @@ const SELECT_ALL_SEARCHES_QUERY = 'SELECT * FROM searches ORDER BY insertTime DE
 
 let searches = [];
 
+function getBookByTitle(title) {
+  return new Promise((resolve, reject) => {
+    axios.get(`https://www.goodreads.com/book/title.xml?&key=9hoCiTrsCBET1EZFyF6F6g&title=${title}`)
+      .then((response) => {
+        parser.parseString(response.data, (err, result) => {
+          if (err) {
+            debug(err);
+          } else {
+            resolve(result.GoodreadsResponse.book);
+          }
+        });
+      })
+      .catch((error) => {
+        process.on('unhandledRejection', (up) => { throw up; });
+
+        // reject(error);
+        debug(error.info);
+        resolve(null);
+      });
+  });
+}
+
+function largerImage(url) {
+  const prefixLen = 'https://images.gr-assets.com/books/1474154022'.length;
+  const res = `${url.substring(0, prefixLen)}l${url.substring(prefixLen + 1)}`;
+  return res;
+}
+
 app.get('/', (req, res) => {
   connection.query(SELECT_ALL_SEARCHES_QUERY, (err, results) => {
     if (err) {
@@ -53,10 +84,10 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-  const name = req.body.bookName;
+  let name = req.body.bookName;
   const insertTime = new Date().getTime();
   const INSERT_SEARCH_QUERY = `INSERT INTO searches (search, insertTime) VALUES('${name}', ${insertTime})`;
-  connection.query(INSERT_SEARCH_QUERY, (err, results) => {
+  connection.query(INSERT_SEARCH_QUERY, async (err, results) => {
     if (err) {
       debug(err);
       return err;
@@ -64,22 +95,50 @@ app.post('/', (req, res) => {
     searches = [name, ...searches];
     const last10 = searches.slice(0, 10);
     const most3 = searches.slice(0, 3);
-    const review = "The Jungle Book: Mowgli's Adventures adapts Rudyard Kipling's timeless tale of a young boy lost in the jungles of India for young children. This delightfully written and illustrated book focuses on the magic of the jungle and the one-of-a-kind characters and fantastical situations that Mowgli finds himself in.";
-    res.render('index', {
-      title: 'Books-Info', name, review, last10, most3
-    });
+    const book = await getBookByTitle(name);
+    debug(book);
+    let review;
+    if (book === null) {
+      res.render('errorHandle', {
+        title: 'Books-Info', name, last10, most3
+      });
+    } else {
+      review = book.description;
+      if (review === '') {
+        review = 'There is no available description for this book.';
+      }
+      name = book.title;
+      res.render('index', {
+        title: 'Books-Info', name, review, last10, most3
+      });
+    }
     return results;
   });
 });
 
-app.get('/:name', (req, res) => {
-  const { name } = req.params;
+app.get('/books/:name', async (req, res) => {
+  let { name } = req.params;
   const last10 = searches.slice(0, 10);
   const most3 = searches.slice(0, 3);
-  const review = "The Jungle Book: Mowgli's Adventures adapts Rudyard Kipling's timeless tale of a young boy lost in the jungles of India for young children. This delightfully written and illustrated book focuses on the magic of the jungle and the one-of-a-kind characters and fantastical situations that Mowgli finds himself in.";
-  res.render('fullInfo', {
-    title: 'Books-Info', review, name, last10, most3
-  });
+  const book = await getBookByTitle(name);
+  let review;
+  let img;
+  if (book === null) {
+    res.render('errorHandle', {
+      title: 'Books-Info', name, last10, most3
+    });
+  } else {
+    review = book.description;
+    if (review === '') {
+      review = 'There is no available description for this book.';
+    }
+    name = book.title;
+    img = largerImage(book.image_url);
+    debug(img);
+    res.render('fullInfo', {
+      title: 'Books-Info', review, name, img, last10, most3
+    });
+  }
 });
 
 app.listen(port, () => {
